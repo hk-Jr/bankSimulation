e4a02a1c251b4e1e4aa747e30968dd7c581c5e55
kishna h k <khk803123@gmail.com>
2025-10-20 14:37:29 +0530

    updated properly.

--- bank.py
+++ bank.py
@@ -1,15 +1,14 @@
-
-initial_balance = int(input("Enter initial balance: ₹"))
+balance_i = int(input("Enter initial balance: ₹"))
 deposit = int(input("Enter deposit amount: ₹"))
 
-new_balance = initial_balance + deposit
-print(f"Initial Balance: Rs.{initial_balance}\nDeposit: Rs.{deposit}\nNew Balance after deposit: Rs.{new_balance}")
+new_b= balance_i + deposit
+print(f"Initial Balance: Rs.{balance_i}\nDeposit: Rs.{deposit}\nNew Balance after deposit: Rs.{new_b}")
 
 #added withdraw feauture.
 withdraw = float(input("Enter withdrawal amount: ₹"))
-if withdraw > new_balance:
+if withdraw > new_b:
     print("Insufficient balance! Withdrawal not possible.")
 else:
-    final_balance = new_balance - withdraw
+    final_b = new_b - withdraw
     print("Withdraw: ₹", withdraw)
-    print("Final Balance: ₹", final_balance)
+    print("Final Balance: ₹", final_b)
