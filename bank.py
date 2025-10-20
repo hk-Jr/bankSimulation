5f561fc711b50656fa0284b527c43b849a991787
kishna h k <khk803123@gmail.com>
2025-10-20 14:08:46 +0530

    this file simulates bank system.

---
+++ bank.py
@@ -0,0 +1,7 @@
+
+initial_balance = int(input("Enter initial balance: ₹"))
+deposit = int(input("Enter deposit amount: ₹"))
+
+new_balance = initial_balance + deposit
+print(f"Initial Balance: Rs.{initial_balance}\nDeposit: Rs.{deposit}\nNew Balance after deposit: Rs.{new_balance}")
+
